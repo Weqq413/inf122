696ab9c3aa62185519ccf0239f016824e8623c71
chenaoh <wquispeq@gmail.com>
2024-01-22 15:32:09 -0400

    commmmmmm

---
+++ portafolio/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Evento from './components/Evento';
+
+function App() {
+  return (
+    <div className="App">
+      <Evento/>
+      
+    </div>
+  );
+}
+
+export default App;
